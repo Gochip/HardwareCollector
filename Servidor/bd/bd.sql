1a2de2d61535361a20b3343de2d3b4173534b7c0
Gochi <germannparisi@gmail.com>
2016-07-04 20:59:44 -0300

    Subo script de base de datos arreglado y funcionando en MySQL

--- Servidor/bd/bd.sql
+++ Servidor/bd/bd.sql
@@ -10,13 +10,13 @@ CREATE TABLE maquinas(
     fecha_alta DATETIME,
     fecha_sincronizacion DATETIME,
     PRIMARY KEY(id)
-)ENGINE=MyInnoDB DEFAULT CHARACTER SET=utf8;
+)ENGINE=InnoDB DEFAULT CHARACTER SET=utf8;
 
 CREATE TABLE componentes(
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     nombre VARCHAR(100),
     PRIMARY KEY(id)
-)ENGINE=MyInnoDB DEFAULT CHARACTER SET=utf8;
+)ENGINE=InnoDB DEFAULT CHARACTER SET=utf8;
 
 CREATE TABLE componentes_x_maquinas(
     id_componente INT NOT NULL,
@@ -25,7 +25,7 @@ CREATE TABLE componentes_x_maquinas(
     PRIMARY KEY(id_componente, id_maquina),
     FOREIGN KEY(id_maquina) REFERENCES maquinas(id),
     FOREIGN KEY(id_componente) REFERENCES componentes(id)
-)ENGINE=MyInnoDB DEFAULT CHARACTER SET=utf8;
+)ENGINE=InnoDB DEFAULT CHARACTER SET=utf8;
 
 CREATE TABLE caracteristicas_x_componentes(
     id_caracteristica INT NOT NULL AUTO_INCREMENT,
@@ -33,44 +33,57 @@ CREATE TABLE caracteristicas_x_componentes(
     nombre VARCHAR(100),
     PRIMARY KEY(id_caracteristica, id_componente),
     FOREIGN KEY(id_componente) REFERENCES componentes(id)
-)ENGINE=MyInnoDB DEFAULT CHARACTER SET=utf8;
+)ENGINE=InnoDB DEFAULT CHARACTER SET=utf8;
 
 CREATE TABLE caracteristicas_x_componentes_x_maquinas(
-    id_maquina INT NOT,
+    id_maquina INT NOT NULL,
     id_componente INT NOT NULL,
     id_caracteristica INT NOT NULL,
-    nombre VARCHAR(100),
+    nombre VARCHAR(100) NOT NULL,
     PRIMARY KEY(id_maquina, id_componente, id_caracteristica),
     FOREIGN KEY(id_maquina, id_componente) REFERENCES componentes_x_maquinas(id_componente, id_maquina),
     FOREIGN KEY(id_componente, id_caracteristicas) REFERENCES caracteristicas_x_componentes(id_caracteristica, id_componente)
-)ENGINE=MyInnoDB DEFAULT CHARACTER SET=utf8;
+)ENGINE=InnoDB DEFAULT CHARACTER SET=utf8;
 
+
+-- Componentes
+
+INSERT INTO componentes (nombre) VALUES ('disco');
+INSERT INTO componentes (nombre) VALUES ('procesador');
+INSERT INTO componentes (nombre) VALUES ('memoria_ram');
+
+SET @id_disco = (SELECT id FROM componentes WHERE nombre='disco');
+SET @id_procesador = (SELECT id FROM componentes WHERE nombre='procesador');
+SET @id_memoria_ram = (SELECT id FROM componentes WHERE nombre='memoria_ram');
+
+
+-- Características
 
 -- Disco duro
-INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (1,'fabricante');
-INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (1,'modelo');
-INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (1,'numero_serie');
-INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (1,'tipo_interfaz'); -- SCSI, IDE, USB, ...
-INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (1,'firmware');
-INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (1,'cantidad_particiones');
-INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (1,'tamanio'); -- Bytes
+INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (@id_disco,'fabricante');
+INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (@id_disco,'modelo');
+INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (@id_disco,'numero_serie');
+INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (@id_disco,'tipo_interfaz'); -- SCSI, IDE, USB, ...
+INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (@id_disco,'firmware');
+INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (@id_disco,'cantidad_particiones');
+INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (@id_disco,'tamanio'); -- Bytes
 
 -- Procesador
-INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (1,'nombre');
-INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (1,'descripcion');
-INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (1,'fabricante');
-INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (1,'arquitectura');
-INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (1,'cantidad_nucleos');
-INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (1,'cantidad_procesadores');
-INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (1,'velocidad'); -- En MHz
-INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (1,'tamanio_cache'); -- En KB
+INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (@id_procesador,'nombre');
+INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (@id_procesador,'descripcion');
+INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (@id_procesador,'fabricante');
+INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (@id_procesador,'arquitectura');
+INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (@id_procesador,'cantidad_nucleos');
+INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (@id_procesador,'cantidad_procesadores');
+INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (@id_procesador,'velocidad'); -- En MHz
+INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (@id_procesador,'tamanio_cache'); -- En KB
 
 -- Memoria ram
-INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (1,'banco');
-INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (1,'tecnologia'); -- EPROM, VRAM, ...
-INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (1,'fabricante');
-INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (1,'numero_serie');
-INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (1,'tamanio_bus_datos'); -- Bits
-INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (1,'velocidad'); -- MHz
-INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (1,'tamanio'); -- Bytes
+INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (@id_memoria_ram,'banco');
+INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (@id_memoria_ram,'tecnologia'); -- EPROM, VRAM, ...
+INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (@id_memoria_ram,'fabricante');
+INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (@id_memoria_ram,'numero_serie');
+INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (@id_memoria_ram,'tamanio_bus_datos'); -- Bits
+INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (@id_memoria_ram,'velocidad'); -- MHz
+INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (@id_memoria_ram,'tamanio'); -- Bytes
 
