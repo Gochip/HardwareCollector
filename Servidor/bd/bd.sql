23de60dc063cba9d586c14eab8a286dbfc6a026c
Gochi <germannparisi@gmail.com>
2016-07-04 19:46:14 -0300

    Primera version de la base de datos SQL.

--- Servidor/bd/bd.sql
+++ Servidor/bd/bd.sql
@@ -2,72 +2,75 @@ DROP DATABASE IF EXISTS hc_bd;
 CREATE DATABASE hc_bd;
 USE hc_bd;
 
-CREATE TABLE IF NOT EXISTS sistemas_operativos(
-	id INT NOT NULL AUTO_INCREMENT,
-	nombre VARCHAR(255),
-	version VARCHAR(50),
-	PRIMARY KEY(id)
-)ENGINE=InnoDB DEFAULT CHARACTER SET=utf8;
+CREATE TABLE maquinas(
+    id INT NOT NULL AUTO_INCREMENT,
+    mac VARCHAR(20),
+    ipv4 VARCHAR(20),
+    ipv6 VARCHAR(200),
+    fecha_alta DATETIME,
+    fecha_sincronizacion DATETIME,
+    PRIMARY KEY(id)
+)ENGINE=MyInnoDB DEFAULT CHARACTER SET=utf8;
 
-CREATE TABLE IF NOT EXISTS maquinas(
-	id VARCHAR(255) NOT NULL,
-	id_sistema_operativo INT NOT NULL,
-	nombre VARCHAR(255) NULL,
-	fecha_alta DATETIME NOT NULL,
-	fecha_sincronizacion DATETIME NULL, /* fecha_ultimo_contacto */
-	PRIMARY KEY(id),
-	FOREIGN KEY (id_sistema_operativo) REFERENCES sistemas_operativos (id)
-		ON DELETE RESTRICT
-		ON UPDATE RESTRICT
-)ENGINE=InnoDB DEFAULT CHARACTER SET=utf8;
+CREATE TABLE componentes(
+    id INT NOT NULL,
+    nombre VARCHAR(100),
+    PRIMARY KEY(id)
+)ENGINE=MyInnoDB DEFAULT CHARACTER SET=utf8;
 
-CREATE TABLE IF NOT EXISTS procesadores(
-	id INT NOT NULL AUTO_INCREMENT,
-	id_maquina VARCHAR(255) NOT NULL,
-	tipo VARCHAR(255) NULL,
-	velocidad VARCHAR(255) NULL,
-	nucleos VARCHAR(255) NULL,
-	PRIMARY KEY(id,id_maquina),
-	FOREIGN KEY (id_maquina) REFERENCES maquinas (id)
-		ON DELETE RESTRICT
-		ON UPDATE RESTRICT
-)ENGINE=InnoDB DEFAULT CHARACTER SET=utf8;
+CREATE TABLE componentes_x_maquinas(
+    id_componente INT NOT NULL,
+    id_maquina INT NOT NULL,
+    nombre VARCHAR(100),
+    PRIMARY KEY(id_componente, id_maquina),
+    FOREIGN KEY(id_maquina) REFERENCES maquinas(id),
+    FOREIGN KEY(id_componente) REFERENCES componentes(id)
+)ENGINE=MyInnoDB DEFAULT CHARACTER SET=utf8;
 
-CREATE TABLE IF NOT EXISTS memorias(
-	id INT NOT NULL AUTO_INCREMENT,
-	id_maquina VARCHAR(255) NOT NULL,
-	capacidad VARCHAR(255) NULL,
-	tipo VARCHAR(255) NULL,
-	descripcion VARCHAR(255) NULL,
-	numero_serial VARCHAR(255) NULL,
-	numero_ranura VARCHAR(255) NULL,
-	velocidad VARCHAR(255) NULL,
-	nombre VARCHAR(255) NULL,
-	PRIMARY KEY(id,id_maquina),
-	FOREIGN KEY (id_maquina) REFERENCES maquinas (id)
-		ON DELETE RESTRICT
-		ON UPDATE RESTRICT
-)ENGINE=InnoDB DEFAULT CHARACTER SET=utf8;
+CREATE TABLE caracteristicas_x_componentes(
+    id_caracteristica INT NOT NULL AUTO_INCREMENT,
+    id_componente INT NOT NULL,
+    nombre VARCHAR(100),
+    PRIMARY KEY(id_caracteristica, id_componente),
+    FOREIGN KEY(id_componente) REFERENCES componentes(id)
+)ENGINE=MyInnoDB DEFAULT CHARACTER SET=utf8;
 
-CREATE TABLE IF NOT EXISTS discos(
-	id INT NOT NULL AUTO_INCREMENT,
-	id_maquina VARCHAR(255) NOT NULL,
-	nombre VARCHAR(255) NULL,
-	fabricante VARCHAR(255) NULL,
-	modelo VARCHAR(255) NULL,
-	descripcion VARCHAR(255) NULL,
-	tipo VARCHAR(255) NULL,
-	tamanio VARCHAR(255) NULL,
-	numero_serial VARCHAR(255) NULL,
-	firmware VARCHAR(255) NULL,
-	cache VARCHAR(255) NULL,
-	arquitectura VARCHAR(255) NULL,
-	procesadores_fisicos VARCHAR(255) NULL,
-	procesadores_logicos VARCHAR(255) NULL,
-	PRIMARY KEY(id,id_maquina),
-	FOREIGN KEY (id_maquina) REFERENCES maquinas (id)
-		ON DELETE RESTRICT
-		ON UPDATE RESTRICT
-)ENGINE=InnoDB DEFAULT CHARACTER SET=utf8;
+CREATE TABLE caracteristicas_x_componentes_x_maquinas(
+    id_maquina INT NOT,
+    id_componente INT NOT NULL,
+    id_caracteristica INT NOT NULL,
+    nombre VARCHAR(100),
+    PRIMARY KEY(id_maquina, id_componente, id_caracteristica),
+    FOREIGN KEY(id_maquina, id_componente) REFERENCES componentes_x_maquinas(id_componente, id_maquina),
+    FOREIGN KEY(id_componente, id_caracteristicas) REFERENCES caracteristicas_x_componentes(id_caracteristica, id_componente)
+)ENGINE=MyInnoDB DEFAULT CHARACTER SET=utf8;
 
-INSERT INTO sistemas_operativos VALUES (1,'Windows','Seven');
+
+-- Disco duro
+INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (1,'fabricante');
+INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (1,'modelo');
+INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (1,'numero_serie');
+INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (1,'tipo_interfaz'); -- SCSI, IDE, USB, ...
+INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (1,'firmware');
+INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (1,'cantidad_particiones');
+INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (1,'tamanio'); -- Bytes
+
+-- Procesador
+INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (1,'nombre');
+INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (1,'descripcion');
+INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (1,'fabricante');
+INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (1,'arquitectura');
+INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (1,'cantidad_nucleos');
+INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (1,'cantidad_procesadores');
+INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (1,'velocidad'); -- En MHz
+INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (1,'tamanio_cache'); -- En KB
+
+-- Memoria ram
+INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (1,'banco');
+INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (1,'tecnologia'); -- EPROM, VRAM, ...
+INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (1,'fabricante');
+INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (1,'numero_serie');
+INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (1,'tamanio_bus_datos'); -- Bits
+INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (1,'velocidad'); -- MHz
+INSERT INTO caracteristicas_x_componentes (id_componente, nombre) VALUES (1,'tamanio'); -- Bytes
+
